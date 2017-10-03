d3324fa72afc007ade2c7f0b9ee76132e139f76d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-03 14:25:08 -0700

    working code

--- game.py
+++ game.py
@@ -4,9 +4,22 @@
 import random
 
 print "Welcome to the game!"
-"""name = raw_input("What's your name?")
+name = raw_input("What's your name?")
 print"Choose a random number"
-num = randint(1, 100)
+secret_num = random.randint(1, 100)
+print " the secret number is %d" %(secret_num)
 guess = None
-while guess != num:"""
+too_high = 100
+too_low = 0
+while guess != secret_num:
+    guess = (too_high + too_low) / 2
+    if guess > secret_num:
+        too_high = guess
+        print "%d too high, guess again" % (guess)
+    if guess < secret_num:
+        too_low = guess
+        print "%d too low, guess again" % (guess)
+
+
+print "The guessed number is %d"%(guess)
 
