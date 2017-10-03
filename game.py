12827c990e0264356ebafc344d7e31f3c02a20b7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-03 15:11:04 -0700

    with user input and range check

--- game.py
+++ game.py
@@ -4,22 +4,33 @@
 import random
 
 print "Welcome to the game!"
-name = raw_input("What's your name?")
-print"Choose a random number"
+name = raw_input("What's your name? ")
+
+print"Choose a random number, %s" % (name)
+
 secret_num = random.randint(1, 100)
-print " the secret number is %d" %(secret_num)
+#print " the secret number is %d" % (secret_num)
+
 guess = None
 too_high = 100
 too_low = 0
+count = 0
+
 while guess != secret_num:
-    guess = (too_high + too_low) / 2
-    if guess > secret_num:
+    count = count + 1
+    guess = int(raw_input("Guess the number: "))
+    if guess > 100 or guess < 0:
+        print "invalid input, WHAT WERE YOU THINKING?! Enter a number between 1 and 100"
+        #guess = int(raw_input("Guess the number: ")) 
+
+    elif guess > secret_num:
         too_high = guess
         print "%d too high, guess again" % (guess)
-    if guess < secret_num:
+
+    elif guess < secret_num:
         too_low = guess
         print "%d too low, guess again" % (guess)
 
 
 print "The guessed number is %d"%(guess)
-
+print "The number of guesses is %d" %(count)
