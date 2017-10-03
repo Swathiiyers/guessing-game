ce8769d9bcf3738b77fbd9d62573888175804d25
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-03 15:46:29 -0700

    adding functions - problem: secret_number is same for all function calls

--- game.py
+++ game.py
@@ -11,29 +11,52 @@ print"Choose a random number, %s" % (name)
 secret_num = random.randint(1, 100)
 #print " the secret number is %d" % (secret_num)
 
-guess = None
-too_high = 100
-too_low = 0
-count = 0
+# guess = None
+# too_high = 100
+# too_low = 0
+# count = 0
 
-while guess != secret_num:
-    count = count + 1
-    try:
-        guess = int(raw_input("Guess the number: "))
-        if guess > 100 or guess < 0:
-            print "invalid input, WHAT WERE YOU THINKING?! Enter a number between 1 and 100"
-            #guess = int(raw_input("Guess the number: ")) 
+def guessing_game():
+    
+    guess = None
+    too_high = 100
+    too_low = 0
+    count = 0
 
-        elif guess > secret_num:
-            too_high = guess
-            print "%d too high, guess again" % (guess)
+    while guess != secret_num:
+        count = count + 1
+        try:
+            guess = int(raw_input("Guess the number: "))
+            if guess > 100 or guess < 1:
+                print "invalid input, WHAT WERE YOU THINKING?! Enter a number between 1 and 100"
+                #guess = int(raw_input("Guess the number: ")) 
 
-        elif guess < secret_num:
-            too_low = guess
-            print "%d too low, guess again" % (guess)
+            elif guess > secret_num:
+                too_high = guess
+                print "%d too high, guess again" % (guess)
 
-    except ValueError:
-        print "Enter a valid number!"
+            elif guess < secret_num:
+                too_low = guess
+                print "%d too low, guess again" % (guess)
 
-print "The guessed number is %d"%(guess)
-print "The number of guesses is %d" %(count)
+        except ValueError:
+            print "Enter a valid number!"
+
+    print "The guessed number is %d"%(guess)
+    print "The number of guesses is %d" %(count)
+    
+
+
+
+
+def continue_game(): 
+    user_choice = raw_input("Would you like to play again? Say 'yes' or 'no'").lower()
+    if user_choice == 'yes':
+        guessing_game()
+    elif user_choice == 'no':
+        return
+
+
+guessing_game()
+continue_game()
+    
