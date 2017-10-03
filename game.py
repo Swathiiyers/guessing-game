e00a92e6191f8060b27ddec24b67397926113824
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-03 12:59:20 -0700

    Greeting the player

---
+++ game.py
@@ -0,0 +1,12 @@
+"""A number-guessing game."""
+
+# Put your code here
+import random
+
+print "Welcome to the game!"
+"""name = raw_input("What's your name?")
+print"Choose a random number"
+num = randint(1, 100)
+guess = None
+while guess != num:"""
+
